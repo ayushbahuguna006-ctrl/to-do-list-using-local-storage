2bf7128c3796b53a658254c855c46a7a00f003b1
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2025-12-31 17:48:31 +0530

    Project Compeleted

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { IoMdAdd } from "react-icons/io";
 import { MdDeleteForever } from "react-icons/md";
@@ -8,29 +8,59 @@ function Main() {
       const [tasks,settasks]=useState([])
       const [ischecked,setischecked]=useState([])
       const handlecheck=(index)=>{
-        setischecked(
-        ischecked.map((item,i)=>{ return i===index?!item:item}))
+       const updatedchecks= ischecked.map((item,i)=>{ return i===index?!item:item})
+       setischecked(updatedchecks);
+       localStorage.setItem("checks",JSON.stringify(updatedchecks))
     
       }
-      const handleclick=()=>{
+      useEffect(()=>{
+        const store=localStorage.getItem("list")
+        const storecheck=localStorage.getItem("checks")
+        if(store){
+                settasks(JSON.parse(store))
+        }
+        if(storecheck){
+            setischecked(JSON.parse(storecheck))
+        }
+      },[])
+      const handleadd=()=>{
         if(task.trim()===""){
               return}
         for(let i=0;i<tasks.length;i++){if(tasks[i].toLowerCase().trim()===task.toLowerCase().trim()){alert("TASK IS BEING REPEATED");return;}}
-        
-          settasks([...tasks,task])
-          setischecked([...ischecked,false])
-          settask("")
           
-        
-      }
+
+          const updatedtask=[...tasks,task]
+          localStorage.setItem("list",JSON.stringify(updatedtask))
+          settasks([...tasks,task])
+         const updatedChecks = [...ischecked, false];
+setischecked(updatedChecks);
+localStorage.setItem("checks", JSON.stringify(updatedChecks));
+          settask("")
+     }
+     const handledelete = (index) => {
+  const updatedTasks = tasks.filter((item, i) => i !== index);
+  const updatedChecks = ischecked.filter((item, i) => i !== index);
+
+  settasks(updatedTasks);
+  setischecked(updatedChecks);
+
+  localStorage.setItem("list", JSON.stringify(updatedTasks));
+  localStorage.setItem("checks", JSON.stringify(updatedChecks));
+};
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex items-center min-h-screen justify-center bg-stone-200'>
       <div className='border-black border backdrop-blur-md h-100  w-[90%] rounded-lg md:w-1/2 lg:w-1/3 mt-10  bg-purple-300 '>
       {/*add button*/}
       <div className='p-10 flex md:justify-around justify-between '><input type="text" placeholder='TASK' value={task} onChange={(e)=>settask(e.target.value)} className='outline-none  hover:border-blue-500 hover:w-55 transition-all duration-100 border border-black w-45 md:w-1/2 p-1 pl-4 rounded-lg font-semibold' /><button className='border-2 bg-blue-400
-       p-1 rounded-lg' onClick={handleclick} ><IoMdAdd className='w-8 active:h-3 w-3'/></button></div>
-       <div>{tasks.map((item,index)=>{return  <p style={{textDecoration:ischecked[index]?"line-through":""}} className="px-10 flex justify-between font-mono mt-2 text-base text-black text-start border-y-1 border-white p-2"><label> <input type="checkbox"  onClick={()=>handlecheck(index)}/> {item.toUpperCase()}</label><button><MdDeleteForever className='mt-0.5 h-6 active:h-6 active:w-5 w-6'/></button></p>})}</div>
-         {/* delete button */}
+       p-1 rounded-lg' onClick={handleadd} ><IoMdAdd className='w-8 active:h-3 w-3'/></button></div>
+       <div>{tasks.map((item,index)=>{return  <p style={{textDecoration:ischecked[index]?"line-through":""}} className="px-10 flex justify-between font-mono mt-2 text-base text-black text-start border-y-1 border-white p-2"><label> <input type="checkbox"   checked={ischecked[index] || false}
+  onClick={()=>handlecheck(index)}/> {item.toUpperCase()}</label>  
+       
+       
+       {/* delete button */}
+       
+       <button onClick={()=>handledelete(index)}><MdDeleteForever className='mt-0.5 h-6 active:h-6 active:w-5 w-6 text-red-600 hover:text-red-800'/></button></p>})}</div>
+       
 
 
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div>
        {/* header */}
-            <div className='flex justify-between md:px-10 bg-violet-500 p-4 items-center'>
+            <div className='flex justify-between md:px-10 bg-gradient-to-r from-purple-600 to-violet-400 p-4 items-center'>
                 <div className='flex'><p className='font-mono text-2xl text-white'>TO-DO</p><p><FaClipboardList className='text-orange-300 mt-1 ml-2 text-2xl' />
       </p></div>
                <div className='flex'> <Link to="/" className='font-mono text-base border-black border-2 pt-1 px-3 text-white rounded-md hover:cursor-pointer hover:bg-violet-900'> HOME</Link>
